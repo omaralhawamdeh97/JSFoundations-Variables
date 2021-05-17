59171508ec4a93aa1dbf389855c30cee5666e432
omaralhawamdeh97 <omar.alhawamdeh@gmail.com>
2021-05-17 15:40:45 +0300

    Done!

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "omar" ;
+let age = 24 ;
+let hoppy = "sleeping" ;
+
+console.log(`${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hoppy} practice!`)
